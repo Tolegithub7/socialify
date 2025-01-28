0a33b9da3ef0d2f8a84d5f8c4635be2c716b87d0
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-28 22:05:20 +0300

    .

--- socialify/src/app/page.tsx
+++ socialify/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
-
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <button className='bg-slate500'></button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
