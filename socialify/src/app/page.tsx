25b328e85c80f198f48afac29b18d1d99830cb57
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-28 22:58:56 +0300

    mode, theme, button components added

--- socialify/src/app/page.tsx
+++ socialify/src/app/page.tsx
@@ -4,17 +4,21 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import ModeToggle from '@/components/ui/Modetoggle'
 export default function Home() {
   return (
     <div>
       <SignedOut>
         <SignInButton mode='modal'>
-          <button className='bg-slate-500 border border-spacing-2'> SignIn </button>
+          <Button> SignIn </Button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <ModeToggle />
+      <Button variant={"secondary"}> Click Me </Button>
     </div>
   );
 }
