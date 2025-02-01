a687360878d9c28d42fe74deee680eb131620f2b
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-02-01 13:05:17 +0300

    add best practice

--- socialify/src/app/page.tsx
+++ socialify/src/app/page.tsx
@@ -1,11 +1,4 @@
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import { Button } from '@/components/ui/button'
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <h1>
