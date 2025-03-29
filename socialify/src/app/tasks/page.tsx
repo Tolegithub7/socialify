3176f6d4c2e97350a5e17ec3c9e66828655d56f1
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-03-29 12:12:58 +0300

    api

---
+++ socialify/src/app/tasks/page.tsx
@@ -0,0 +1,43 @@
+async function TasksPage() {
+    const response = await fetch("http://localhost:3000/api/tasks", {
+        cache: "no-store",
+        next: {
+            revalidate: 0,
+        },
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        method: "GET",
+        // body: JSON.stringify({}),
+    });
+    const tasks = await response.json();
+    console.log(tasks);
+  return (
+    <div>
+      Tasks Page
+    </div>
+  )
+}
+
+export default TasksPage;
+
+// async function TasksPage() {
+//     const response = await fetch("http://localhost:3000/api/tasks");
+
+//     if (!response.ok) {
+//         console.error("Error fetching tasks:", response.status, response.statusText);
+//         return <div>Error loading tasks.</div>;
+//     }
+
+//     try {
+//         const tasks = await response.json();
+//         console.log(tasks);
+//         return <div>Tasks Page</div>;
+//     } catch (error) {
+//         console.error("JSON parsing error:", error);
+//         return <div>Error parsing tasks.</div>;
+//     }
+// }
+
+// export default TasksPage;
